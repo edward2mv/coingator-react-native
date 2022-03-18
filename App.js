293281959bcd2589844a438e682451a87ce161a8
edward2mv <edward.emv@gmail.com>
2022-03-17 21:47:01 -0400

    readme

--- App.js
+++ App.js
@@ -1,20 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  Image,
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  FlatList,
+  TextInput,
+} from "react-native";
 
-export default function App() {
+import CoinItem from "./components/CoinItem";
+//import logo from '../../assets/coingator-white-v2.png'
+
+const App = () => {
+  const [coins, setCoins] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const [search, setSearch] = useState("");
+
+  const loadData = async () => {
+    const res = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    );
+    const data = await res.json();
+    setCoins(data);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor="#141414" />
+      <View style={styles.header}>
+        <Image
+            style={styles.logo}
+            source={require('./assets/coingator-wihite-v2.png')}
+          />
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search a Coin"
+          placeholderTextColor="#858585"
+          onChangeText={(text) => text && setSearch(text)}
+        />
+      </View>
+
+      <FlatList
+        style={styles.list}
+        data={coins.filter(
+          (coin) =>
+            coin.name.toLowerCase().includes(search.toLocaleLowerCase()) ||
+            coin.symbol.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+        )}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => <CoinItem coin={item} />}
+        refreshing={refreshing}
+        onRefresh={async () => {
+          setRefreshing(true);
+          await loadData();
+          setRefreshing(false);
+        }}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#141414",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+  },
+  header: {
+    flexDirection: "row",
+    width: "90%",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 20,
+    color: "#fff",
+    marginTop: 10,
+  },
+  logo: {
+    marginLeft: -30,
+    marginTop: 10,
+      resizeMode: "contain",
+            height: 100,
+            width: 200,
+  },
+  list: {
+    width: "90%",
+  },
+  searchInput: {
+    color: "#fff",
+    borderBottomColor: "#4657CE",
+    borderBottomWidth: 1,
+    width: "40%",
+    textAlign: "center",
   },
 });
+
+export default App;
